5f1c7994c960b8c6a550c568201179413b84f6cc
caroww <caroline.ww@wanadoo.fr>
2020-12-06 09:19:50 +0100

    11.Form

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,8 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\CategoryType;
 use App\Entity\Category;
 use App\Entity\Program;
+
+
 
 /**
  * @Route("/categories", name="category_")
@@ -31,6 +35,39 @@ class CategoryController extends AbstractController
     }
 
      /**
+     * The controller for the category add form
+     * Display the form or deal with it
+     *
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request) : Response
+    {
+        // Create a new Category Object
+        $category = new Category();
+        // Create the associated Form
+        $form = $this->createForm(CategoryType::class, $category);
+        // Get data from HTTP request
+        $form->handleRequest($request);
+        // Was the form submitted ?
+        if ($form->isSubmitted()) {
+            // Deal with the submitted data
+            // Get the Entity Manager
+            $entityManager = $this->getDoctrine()->getManager();
+            // Persist Category Object
+            $entityManager->persist($category);
+            // Flush the persisted object
+            $entityManager->flush();
+            // Finally redirect to categories list
+            return $this->redirectToRoute('category_index');
+        }    
+              
+        // Render the form
+        return $this->render('category/new.html.twig', [
+            "form" => $form->createView(),
+        ]);
+    } 
+    
+        /**
      * Getting a program by category
      *
      * @Route("/{categoryName}", name="show")
